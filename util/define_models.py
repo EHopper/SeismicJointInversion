Weighting equations all seem to be working....!  Next up: play around with scaling, and fix the thickness params better.  Still for the future once that's all working (ish) - add RF constraints.

util/define_models.py
@@ -537,7 +537,8 @@ def _set_model_indices(setup_model, model, **kwargs):
             - If Moho and LAB are not specified in
     """
 
-    depth = list(np.cumsum(model.thickness))
+    # Last model point is not included in the inversion
+    depth = list(np.cumsum(model.thickness[:-1]))
 
     if 'Moho' in setup_model.boundary_names:
         moho_ind = model.boundary_inds[setup_model.boundary_names.index('Moho')]
@@ -564,6 +565,6 @@ def _set_model_indices(setup_model, model, **kwargs):
         lithospheric_mantle = np.arange(moho_ind, lab_ind),
         asthenosphere = np.arange(lab_ind, len(depth)),
         discontinuties = np.array([moho_ind // 2, moho_ind,
-                                   lab_ind, len(depth)]),
+                                   lab_ind, len(depth) - 1]),
         depth = depth
     )

util/inversion.py
@@ -72,9 +72,10 @@ def _inversion_iteration(setup_model:define_models.SetupModel,
     )
     # Can vary other parameters in MINEOS by putting them as inputs to this call
     # e.g. defaults include l_min, l_max; qmod_path; phase_or_group_velocity
+    periods = data.surface_waves.period.values
     params = mineos.RunParameters(freq_max = 1000 / min(periods) + 1)
     ph_vel_pred, kernels = mineos.run_mineos_and_kernels(
-        params, data.surface_waves.Period.values, setup_model.id
+        params, periods, setup_model.id
     )
     kernels = kernels[kernels['z'] <= setup_model.depth_limits[1]]
 
@@ -83,7 +84,7 @@ def _inversion_iteration(setup_model:define_models.SetupModel,
     )
     p = _build_model_vector(model)
     d = _build_data_misfit_matrix(
-        data.surface_waves.Phase_vel.values, ph_vel_pred, p, G
+        data.surface_waves.ph_vel.values, ph_vel_pred, p, G
     )
 
     # Build all of the weighting functions for damped least squares
@@ -93,9 +94,9 @@ def _inversion_iteration(setup_model:define_models.SetupModel,
     )
 
     # Perform inversion
-    model_new = _damped_least_squares(p, G, d, W, D_mat, d_vec, H_mat, h_vec)
+    p_new = _damped_least_squares(p, G, d, W, D_mat, d_vec, H_mat, h_vec)
 
-    return _build_inversion_model_from_model_vector(model_new)
+    return _build_inversion_model_from_model_vector(p_new, model)
 
 
 def _build_model_vector(model:define_models.InversionModel,
@@ -249,20 +250,36 @@ def _damped_least_squares(m0, G, d, W, D_mat, d_vec, H_mat, h_vec):
                     (n_data_points x 1)
         - <m>:  old (a priori) model
                     (n_model_points x 1)
+                    Note that in our final inversion, we do not use this
+                    explicitly as in the notation below, but is included here
+                    to work forwards from the Menke (2012) formulation.
     Where
         - D:    roughness matrix
                     (n_smoothing_equations x n_model_points)
 
     This is formulated in Menke (2012; eq. 3.46) as
         F * m_est  = f
-        F  	=   [    sqrt(We) * G ;    εD   ]
-        f 	=   [    sqrt(We) * d ;    εD<m>   ]
+        F  =  [[  sqrt(We) * G  ]       f   =  [[    sqrt(We) * d    ]
+               [      εD        ]]              [       εD<m>        ]]
+
 
         i.e. m_est = (F' * F)^-1 * F' * f
                    = [(G' * We * G) + (ε^2 * D' * D)]^-1
                      * [(G' * We * d) + (ε^2 * D' * D * <m>) ]
 
-    Then add in any a priori constraints, formulated by Menke (2012; eq. 3.55)
+    Note that here the old model, <m>, is just standing in for the constraints
+    on the smoothness, here explicitly picked out as εD * m_est = εD * <m>.
+    In this formulation, we can just sweep this smoothing constraint in with
+    all of the other a priori constraints.  To damp towards a perfectly smooth
+    model (as coded in weights.py), D approximates the second derivative of
+    the model with D * m_est and is set equal to zero instead of εD * <m>.
+        F  =  [[  sqrt(We) * G  ]       f   =  [[    sqrt(We) * d    ]
+               [      εD        ]]              [  [[0],[0],...,[0]] ]]
+
+        i.e. m_est = (F' * F)^-1 * F' * f
+                   = [(G' * We * G) + (ε^2 * D' * D)]^-1 * [(G' * We * d)]
+
+    Any other a priori constraints, formulated by Menke (2012; eq. 3.55)
     as linear constraint equations:
         H * m = h
 
@@ -270,10 +287,17 @@ def _damped_least_squares(m0, G, d, W, D_mat, d_vec, H_mat, h_vec):
     and h would be the original model.
 
     These are combined as vertical stacks:
-        all damping matrices = [εD; H]
-        all damping results = [εD<m>; h]
-
-
+        F * m_est = f
+        F  =  [[  sqrt(We) * G  ]       f   =  [[    sqrt(We) * d    ]
+               [      εD        ]               [  [[0],[0],...,[0]] ]
+               [      εH        ]]              [         h          ]]
+               ((n_data_points                  ((n_data_points
+                + n_smoothing_equations          + n_smoothing_equations
+                + n_a_priori_constraints),       + n_a_priori_constraints),
+                n_model_params) np.array         1) np.array
+        i.e. m_est = (F' * F)^-1 * F' * f
+                   = [(G' * We * G) + (ε^2 * D' * D) + (H' * H)]^-1
+                     * [(G' * We * d) + (H' * h)]
     """
 
     H = np.vstack((D_mat, H_mat))

util/weights.py
@@ -126,7 +126,6 @@ def _build_weighting_matrices(data:constraints.Observations, m0:np.array,
 
 
 def _set_layer_values(layers, vals):
-    print(layers)
 
     if len(vals) == 1:
         val, = vals
@@ -209,6 +208,7 @@ def _damp_constraints(H, h, layers, damping):
 
     # Loop through all layers
     for layer_name in layers.layer_names:
+
         layer_inds = getattr(layers, layer_name).astype(int)
         layer_damping = getattr(damping, layer_name)
 
@@ -220,7 +220,7 @@ def _damp_constraints(H, h, layers, damping):
         # wrong orientation - have to add an axis (cannot transpose as
         # it starts off as 1D)
         H[:, layer_inds] *= layer_damping
-        h[layer_inds] *= layer_damping[:, np.newaxis]
+        #h[layer_inds] *= layer_damping[:, np.newaxis]
 
     return H, h
 
@@ -279,13 +279,13 @@ def _build_smoothing_constraints(
 
     Returns:
         roughness_mat
-            - (n_depth_points, n_depth_points) np.array
+            - (n_smoothing_equations, n_model_params) np.array
             - Roughness matrix, D, in D * m = d.
             - In Menke (2012)'s notation, this is D and Wm is D^T * D.
             - This is the matrix that we multiply by the model to find the
               roughness of the model (i.e. the second derivative of the model).
         - roughness_vec
-            - (n_depth_points, 1) np.array
+            - (n_smoothing_equations, 1) np.array
             - Roughness vector, d, in D * m = d.
             - This is the permitted roughness of the model.
             - Actual roughness is calculated as roughness_matrix * model, and
@@ -296,7 +296,7 @@ def _build_smoothing_constraints(
               roughness is set to zero always.
 
     """
-    n_depth_points = model.vsv.size
+    n_depth_points = model.vsv.size - 1
 
     # Make and edit the banded matrix (roughness equations)
     banded_matrix = _make_banded_matrix(n_depth_points, (1, -2, 1))
@@ -430,15 +430,15 @@ def _build_constraint_damp_original_gradient(
             - Set to zero always.
     """
 
-    n_depth_points = model.vsv.size
+    n_depth_points = model.vsv.size - 1
 
     H = np.zeros((n_depth_points - 1,
                   n_depth_points + model.boundary_inds.size))
+
     for i_d in range(n_depth_points - 1):
         # Need to transpose slice of m0 (2, 1) to fit in H (1, 2) gap,
         # because in H, depth increases with column not row.
         H[i_d, [i_d, i_d+1]] = 1 / model.vsv[[i_d+1, i_d]].T
-
 
     # Remove constraints around discontinuities between layers.
     do_not_damp = np.concatenate(