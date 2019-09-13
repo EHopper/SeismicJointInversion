Damping is working!! Fixed roughness (do not scale differently except where separated by two unsmoothed nodes!!) and model gradient (had typed the equation in wrong...) although no test written for model graident damping yet. Visual tests via working.test_damping() seem to be pretty promising!
Next stop, RF constraints!  (And more unittests......!)

test_pipeline.py
@@ -796,7 +796,7 @@ class PipelineTest(unittest.TestCase):
                                   expected_s, expected_t):
         damp_s = pd.DataFrame({'Depth': layers.depth})
         damp_t = pd.DataFrame({'Depth': np.arange(len(layers.boundary_layers))})
-        weights._set_layer_values(name, damping, layers, damp_s, damp_t)
+        weights._set_layer_values(damping, layers, damp_s, damp_t, name)
 
         pd.testing.assert_frame_equal(damp_s, expected_s)
         pd.testing.assert_frame_equal(damp_t, expected_t)
@@ -866,21 +866,24 @@ class PipelineTest(unittest.TestCase):
             'simple',
             define_models.InversionModel(
                 vsv = np.array([3] * 13)[:, np.newaxis],
-                thickness = np.array([6] * 13)[:, np.newaxis],
+                thickness = np.array(
+                    [0] + [6] * 3
+                    + [7, 5, 4] + [6] * 1
+                    + [10, 15, 12] + [6] * 1)[:, np.newaxis],
                 boundary_inds = np.array([4, 8])
             ),
             np.array([
                 [0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
                 [1, -2,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
                 [0,  1, -2,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-                [0,  0,  1, -2,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0],
+                [0,  0,  1, -13/7,  6/7,  0,  0,  0,  0,  0,  0,  0,  0,  0],
                 [0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
                 [0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-                [0,  0,  0,  0,  0,  1, -2,  1,  0,  0,  0,  0,  0,  0],
-                [0,  0,  0,  0,  0,  0,  1, -2,  1,  0,  0,  0,  0,  0],
+                [0,  0,  0,  0,  0,  6/4, -10/4,  1,  0,  0,  0,  0,  0,  0],
+                [0,  0,  0,  0,  0,  0,  1, -16/10,  6/10,  0,  0,  0,  0,  0],
                 [0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
                 [0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-                [0,  0,  0,  0,  0,  0,  0,  0,  0,  1, -2,  1,  0,  0],
+                [0,  0,  0,  0,  0,  0,  0,  0,  0,  6/12, -18/12,  1,  0,  0],
                 [0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
             ]),
             np.array([0] * 12)[:, np.newaxis],
@@ -895,17 +898,17 @@ class PipelineTest(unittest.TestCase):
         np.testing.assert_array_equal(H, expected_H)
         np.testing.assert_array_equal(h, expected_h)
 
-        damp_s = pd.DataFrame({'Depth': np.cumsum(model.thickness[:-1])})
-        damp_t = pd.DataFrame({'Depth': [35., 80.]})
         n_layers = expected_h.shape[0]
         n_bls = model.boundary_inds.shape[0]
+        damp_s = pd.DataFrame({'Depth': np.cumsum(model.thickness)})
+        damp_t = pd.DataFrame({'Depth': [0] * n_bls})
         layers = define_models.ModelLayerIndices(
             np.arange(2), np.arange(2, 3), np.arange(3, 4),
             np.arange(4, n_layers), np.arange(n_layers, n_layers + n_bls),
-            np.arange(0, 10)
+            np.arange(0)
         )
         weights._set_layer_values(
-            'roughness', (1, 1, 1, 1, 10), layers, damp_s, damp_t
+            (1, 1, 1, 1, 10), layers, damp_s, damp_t, 'roughness',
         )
         H_sc, h_sc = weights._damp_constraints((H, h, label), damp_s, damp_t)
         self.assertEqual(label, 'roughness')

util/define_models.py
@@ -562,9 +562,9 @@ def _set_model_indices(setup_model, model, **kwargs):
 
     return ModelLayerIndices(
         upper_crust = np.arange(moho_ind/2),
-        lower_crust = np.arange(round(moho_ind/2), moho_ind),
-        lithospheric_mantle = np.arange(moho_ind, lab_ind),
-        asthenosphere = np.arange(lab_ind, len(depth)),
+        lower_crust = np.arange(round(moho_ind/2), moho_ind + 1),
+        lithospheric_mantle = np.arange(moho_ind + 1, lab_ind + 1),
+        asthenosphere = np.arange(lab_ind + 1, len(depth)),
         boundary_layers = len(depth) + np.arange(len(model.boundary_inds)),
         depth = list(depth)
     )

util/weights.py
@@ -56,14 +56,7 @@ def build_weighting_damping(data:constraints.Observations, p:np.array,
 
     # Record level of damping on the Vsv model parameters
     layers = define_models._set_model_indices(setup_model, model)
-    damp_s = pd.DataFrame({
-        'Depth': np.mean(
-            np.vstack(
-                (layers.depth,
-                layers.depth[1:] + [np.sum(model.thickness)])
-                ),
-            0),
-        })
+    damp_s = pd.DataFrame({'Depth': layers.depth})
     # Record level of damping on the boundary layer depth parameters
     damp_t = pd.DataFrame({
         'Depth': np.cumsum(model.thickness)[list(model.boundary_inds)],
@@ -71,19 +64,32 @@ def build_weighting_damping(data:constraints.Observations, p:np.array,
 
 
     # Minimise second derivative within layers - roughness
-    _set_layer_values('roughness', (1, 1, 1, 1, 1), layers, damp_s, damp_t)
+    # Note that because we are smoothing across upper-crust to lower-crust,
+    # having different values for those two layers messes with things, as does
+    # having variable damping within a layer
+    # Already built into roughness_mat is that we do not smooth around BLs
+    _set_layer_values((1, 1, 1, 2, 0), layers, damp_s, damp_t, 'roughness')
     roughness_mat, roughness_vec = _damp_constraints(
         _build_smoothing_constraints(model), damp_s, damp_t
     )
 
     # Linear constraint equations
     # Damp towards starting model
-    _set_layer_values('to_m0', (0, 0, 0, 0, 0), layers, damp_s, damp_t)
+    _set_layer_values(
+        (
+            [0.5] * len(layers.upper_crust),
+            [0.5] * (len(layers.lower_crust) - 1) + [0.5],
+            [0.5] * (len(layers.lithospheric_mantle) - 1) + [0.5],
+            [0.5] * len(layers.asthenosphere),
+            [0.5] * len(layers.boundary_layers)
+        ),
+        layers, damp_s, damp_t, 'to_m0'
+    )
     damp_to_m0_mat, damp_to_m0_vec = _damp_constraints(
         _build_constraint_damp_to_m0(p), damp_s, damp_t
     )
     # Damp towards starting model gradients in Vs
-    _set_layer_values('to_m0_grad', (0, 0, 0, 0, 0), layers, damp_s, damp_t)
+    _set_layer_values((1, 1, 1, 1, 0), layers, damp_s, damp_t, 'to_m0_grad')
     damp_to_m0_grad_mat, damp_to_m0_grad_vec = _damp_constraints(
         _build_constraint_damp_original_gradient(model), damp_s, damp_t
     )
@@ -108,7 +114,7 @@ def build_weighting_damping(data:constraints.Observations, p:np.array,
     return W, a_priori_mat, a_priori_vec
 
 
-def _set_layer_values(label, damping, layers, damp_s, damp_t):
+def _set_layer_values(damping, layers, damp_s, damp_t, label):
 
     damp_s[label] = 0
     damp_t[label] = damping[-1]
@@ -117,6 +123,7 @@ def _set_layer_values(label, damping, layers, damp_s, damp_t):
     n = 0
     for layer_name in layers.layer_names:
         if layer_name == 'boundary_layers':
+            n += 1
             continue
         layer_inds = getattr(layers, layer_name).astype(int)
         damp_s.loc[layer_inds, label] =  damping[n]
@@ -449,16 +456,14 @@ def _build_constraint_damp_original_gradient(
     for i_d in range(n_depth_points - 1):
         # Need to transpose slice of m0 (2, 1) to fit in H (1, 2) gap,
         # because in H, depth increases with column not row.
-        H[i_d, [i_d, i_d+1]] = 1 / model.vsv[[i_d+1, i_d]].T
+        H[i_d, [i_d, i_d+1]] = np.hstack((
+            1 / model.vsv[i_d], -1 / model.vsv[i_d + 1]
+        ))
+    H[-1, -1] = 1 / model.vsv[-2] - 1
 
-    # Remove constraints around discontinuities between layers.
-    do_not_damp = np.concatenate(
-        (np.array([0, -1]), model.boundary_inds, model.boundary_inds + 1)
-    ).astype(int)
-    for d in do_not_damp:
-        H[d,:] *= 0
-        if d > 0:
-            H[d-1, :] *= 0
+    # Remove constraints around discontinuities
+    do_not_damp = list(model.boundary_inds)
+    H[do_not_damp, :] = 0
 
     h = np.zeros((n_depth_points - 1, 1))
 

util/working.py
@@ -141,10 +141,10 @@ def test_damping(n_iter):
     )
     data = constraints.extract_observations(35, -104)
     # To speed things up, remove some data
-    data = data._replace(surface_waves =
-        data.surface_waves.iloc[[3, 7, 9, 11, 12, 13], :].reset_index()
-    )
-    setup_model = setup_model._replace(boundary_names = [])
+    # data = data._replace(surface_waves =
+    #     data.surface_waves.iloc[[3, 7, 9, 11, 12, 13], :].reset_index()
+    # )
+    #setup_model = setup_model._replace(boundary_names = [])
     periods = data.surface_waves.period.values
     save_name = 'output/{0}/{0}.q'.format(setup_model.id)
 
@@ -157,10 +157,11 @@ def test_damping(n_iter):
     line.set_label('data')
 
     m = define_models.setup_starting_model(setup_model)
-    m = m._replace(boundary_inds =  np.array([]))
-    m = m._replace(thickness=np.array([0.] + [6.] * (len(m.vsv) - 1))[:, np.newaxis])
+    # m = m._replace(boundary_inds =  np.array([]))
+    # m = m._replace(thickness=np.array([0.] + [6.] * (len(m.vsv) - 1))[:, np.newaxis])
     plots.plot_model(m, 'm0', ax_m)
     for n in range(n_iter):
+        print('****** ITERATION ' +  str(n) + ' ******')
         m = inversion._inversion_iteration(setup_model, m, data)
         c = mineos._read_qfile(save_name, periods)
         plots.plot_model(m, 'm' + str(n + 1), ax_m)